d970635dccf7bf29d6c9a9bb1b3112c916706b5f
limonzishad <zishadlimon@gmail.com>
2022-06-09 22:56:45 +0600

    projects added in home page. footer position issue wasn't solved.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Education from "./components/Education";
 import Footer from "./components/Footer";
 
 const App = () => {
@@ -8,6 +10,8 @@ const App = () => {
     <div>
       <NavBar></NavBar>
       <Home></Home>
+      <Projects></Projects>
+      <Education></Education>
       <Footer></Footer>
     </div>
   );
---
+++ src/components/Projects.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "../css/Projects.css";
+import Electrool from "../assets/Electrool.png";
+import HandsetStore from "../assets/HandsetStore.png";
+import OnlineTutor from "../assets/OnlineTutor.png";
+
+const Projects = () => {
+    return (
+        <div id="projects">
+            <h1 className="text-center title">MY PROJECTS</h1>
+
+            <div className="project-container">
+                <div className="project">
+                    <div className="project-image">
+                        <img src={Electrool} alt="Electrool.png" />
+                    </div>
+
+                    <div className="project-details">
+                        <h2>ELECTROOL</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ex dolorem veritatis expedita quis reiciendis, quae sed. Aut optio nesciunt, totam quae quos quam animi, iure laboriosam consectetur incidunt hic.</p>
+                        <div>
+                            <a href="https://electrool.web.app/"><button className="live-site">LIVE SITE</button></a>
+                            <a href="https://github.com/limonzishad/electrool-client"><button className="client-link">GITHUB [CLIENT]</button></a>
+                            <a href="https://github.com/limonzishad/electrool-server"><button className="server-link">GITHUB [SERVER]</button></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="project">
+                    <div className="project-image">
+                        <img src={HandsetStore} alt="HandsetStore.png" />
+                    </div>
+
+                    <div className="project-details">
+                        <h2>HANDSET STORE</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ex dolorem veritatis expedita quis reiciendis, quae sed. Aut optio nesciunt, totam quae quos quam animi, iure laboriosam consectetur incidunt hic.</p>
+                        <div>
+                            <a href="https://handset-store.web.app/"><button className="live-site">LIVE SITE</button></a>
+                            <a href="https://github.com/limonzishad/handset-store"><button className="client-link">GITHUB [CLIENT]</button></a>
+                            <a href="https://github.com/limonzishad/handset-store-server"><button className="server-link">GITHUB [SERVER]</button></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="project">
+                    <div className="project-image">
+                        <img src={OnlineTutor} alt="OnlineTutor.png" />
+                    </div>
+
+                    <div className="project-details">
+                        <h2>ONLINE TUTOR</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ex dolorem veritatis expedita quis reiciendis, quae sed. Aut optio nesciunt, totam quae quos quam animi, iure laboriosam consectetur incidunt hic.</p>
+                        <div>
+                            <a href="https://online-tutor-dba75.web.app/"><button className="live-site">LIVE SITE</button></a>
+                            <a href="https://github.com/limonzishad/online-tutor"><button className="client-link">GITHUB</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Projects;
