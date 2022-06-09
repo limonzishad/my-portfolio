1f5842576ca846790717eae72f5f7b49dfcba0c2
limonzishad <zishadlimon@gmail.com>
2022-06-09 16:05:12 +0600

    footer completed.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <NavBar></NavBar>
       <Home></Home>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Card } from "react-bootstrap";
+import Facebook from "../assets/Facebook.png";
+import Github from "../assets/Github.png";
+import LinkedIn from "../assets/LinkedIn.png";
+import "../css/Footer.css";
 
 const Footer = () => {
     return (
-        <div>
-
-        </div>
+        <Card className="bg-dark text-white text-center">
+            <Card.Header>LAST BUT NOT THE LEAST</Card.Header>
+            <Card.Body>
+                <Card.Text>Thank you for visiting my portfolio</Card.Text>
+                <Card.Title>SOCIAL LINKS</Card.Title>
+                <div className="social-icons">
+                    <Card.Text><a href="https://www.facebook.com/limonzishad/" target="_blank"><img src={Facebook} alt="github_icon" /></a></Card.Text>
+                    <Card.Text><a href="https://github.com/limonzishad" target="_blank"><img src={Github} alt="github_icon" /></a></Card.Text>
+                    <Card.Text><a href="https://www.linkedin.com/in/limonzishad/" target="_blank"><img src={LinkedIn} alt="github_icon" /></a></Card.Text>
+                </div>
+            </Card.Body>
+            <Card.Footer className="bg-dark text-white">&copy; copyright 2022. All right reserve to zishadlimon@gmail.com</Card.Footer>
+        </Card>
     );
 }
 
