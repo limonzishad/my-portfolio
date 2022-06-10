36262fc436f7728b8d89430bff5065c2ccec91f1
limonzishad <zishadlimon@gmail.com>
2022-06-10 21:41:35 +0600

    ui updated.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import "../css/Projects.css";
 import Electrool from "../assets/Electrool.png";
 import HandsetStore from "../assets/HandsetStore.png";
 import OnlineTutor from "../assets/OnlineTutor.png";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
     return (
@@ -17,14 +18,14 @@ const Projects = () => {
 
                     <div className="project-details">
                         <h2>ELECTROOL</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ex dolorem veritatis expedita quis reiciendis, quae sed. Aut optio nesciunt, totam quae quos quam animi, iure laboriosam consectetur incidunt hic.</p>
+                        <p>Electrool is a manufacturers website where byers can buy products (electric tools) from them. Users can Register and Login to the website using Email address or with Google accounts and users will receive a verification email after successful registration. Users can order products if the product is available.</p>
                         <div className="button-group">
                             <a href="https://electrool.web.app/" target="_blank" rel="noreferrer"><button className="live-site">LIVE SITE</button></a>
                             <a href="https://github.com/limonzishad/electrool-client" target="_blank" rel="noreferrer"><button className="git-link">CLIENT</button></a>
                             <a href="https://github.com/limonzishad/electrool-server" target="_blank" rel="noreferrer"><button className="git-link">SERVER</button></a>
                         </div>
                         <div>
-                            <a href=""><button className="details-button">DETAILS</button></a>
+                            <Link to=""><button className="details-button">DETAILS</button></Link>
                         </div>
                     </div>
                 </div>
@@ -36,14 +37,14 @@ const Projects = () => {
 
                     <div className="project-details">
                         <h2>HANDSET STORE</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ex dolorem veritatis expedita quis reiciendis, quae sed. Aut optio nesciunt, totam quae quos quam animi, iure laboriosam consectetur incidunt hic.</p>
+                        <p>Handset Store is a warehouse or inventory based website for mobile/smart devices. Users can Register and Login to the website using Email address or with Google accounts. Users will receive a verification email after successful registration. Besides, users can reset their password. Also, there are some protected pages which require user Login to visit those pages.</p>
                         <div className="button-group">
                             <a href="https://handset-store.web.app/" target="_blank" rel="noreferrer"><button className="live-site">LIVE SITE</button></a>
                             <a href="https://github.com/limonzishad/handset-store" target="_blank" rel="noreferrer"><button className="git-link">CLIENT</button></a>
                             <a href="https://github.com/limonzishad/handset-store-server" target="_blank" rel="noreferrer"><button className="git-link">SERVER</button></a>
                         </div>
                         <div>
-                            <a href=""><button className="details-button">DETAILS</button></a>
+                            <Link to=""><button className="details-button">DETAILS</button></Link>
                         </div>
                     </div>
                 </div>
@@ -55,13 +56,13 @@ const Projects = () => {
 
                     <div className="project-details">
                         <h2>ONLINE TUTOR</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ex dolorem veritatis expedita quis reiciendis, quae sed. Aut optio nesciunt, totam quae quos quam animi, iure laboriosam consectetur incidunt hic.</p>
+                        <p>Online Tutor is a website for an individual person (teacher) who provides services (courses). Users can Register and Login to the website using Email address or with Google accounts. Users will receive a verification email after successful registration. Besides, user can reset their password.</p>
                         <div className="button-group">
                             <a href="https://online-tutor-dba75.web.app/" target="_blank" rel="noreferrer"><button className="live-site">LIVE SITE</button></a>
                             <a href="https://github.com/limonzishad/online-tutor" target="_blank" rel="noreferrer"><button className="git-link">GITHUB</button></a>
                         </div>
                         <div>
-                            <a href=""><button className="details-button">DETAILS</button></a>
+                            <Link to=""><button className="details-button">DETAILS</button></Link>
                         </div>
                     </div>
                 </div>
